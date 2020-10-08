d067b607ae00da5d497fb3935a529a9268ea6007
LashanaTello <lashana.tello@gmail.com>
2020-10-08 14:08:21 -0400

    Navbar displays welcome message and shows logout button

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,61 @@
 import React, { Component } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../store/actions/authActions';
 
 class NavBar extends Component {
+  handleLogout = (e) => {
+    e.preventDefault();
+    this.props.logoutUser();
+    this.props.history.push("/");
+  };
+
   render() {
-    return (
-      <nav className="nav-wrapper" style={{backgroundColor: "#057481"}}>
-        <div className="container">
-          <Link to="/" className="brand-logo">Pocket Stocks</Link>
-          <ul className="right">
-            <li><NavLink to="/portfolio">Portfolio</NavLink></li>
-            <li><NavLink to="/transactions">Transactions</NavLink></li>
-          </ul>
-        </div>
-      </nav>
-    );
+    if (this.props.auth.isAuthenticated) {
+      const { user } = this.props.auth;
+      return (
+        <nav className="nav-wrapper" style={{backgroundColor: "#057481"}}>
+          <div className="container">
+            <Link to="/" className="brand-logo">Pocket Stocks</Link>
+            <ul className="right">
+              <li>Welcome, { user.name }</li>
+              <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+              <li><NavLink to="/transactions">Transactions</NavLink></li>
+              <button 
+                className="btn btn-large waves-effect waves-light hoverable" 
+                style={{backgroundColor: "#f49f0a"}} 
+                onClick={this.handleLogout}
+              >
+                Logout
+              </button>
+            </ul>
+          </div>
+        </nav>
+      );
+    } else {
+      return (
+        <nav className="nav-wrapper" style={{backgroundColor: "#057481"}}>
+          <div className="container">
+            <Link to="/" className="brand-logo">Pocket Stocks</Link>
+            <ul className="right">
+              <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+              <li><NavLink to="/transactions">Transactions</NavLink></li>
+            </ul>
+          </div>
+        </nav>
+      );
+    }
   }
 }
 
-export default NavBar;
+NavBar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(withRouter(NavBar));
