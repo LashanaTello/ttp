8679a2f553f54730783d56681bb13e6181458eaf
LashanaTello <lashana.tello@gmail.com>
2020-10-19 20:42:13 -0400

    Updates

---
+++ precache-manifest.299b63ccbcd256d366b42486faea9233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6b563f6780ebe0476da56a8ee5d64d",
+    "url": "/ttp/index.html"
+  },
+  {
+    "revision": "0901fdfde377d49dd000",
+    "url": "/ttp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9ea78a4f62f68acc794c",
+    "url": "/ttp/static/js/2.5cb42f4d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ttp/static/js/2.5cb42f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0901fdfde377d49dd000",
+    "url": "/ttp/static/js/main.bca1941f.chunk.js"
+  },
+  {
+    "revision": "e033346eff585ac1d6ae",
+    "url": "/ttp/static/js/runtime-main.8cadd502.js"
+  }
+]);
